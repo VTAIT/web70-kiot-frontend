21cbdc271891646674e0f346ca93a0da17c23e8a
Lan Pham <lan.pham@dfs.de>
2023-08-24 06:44:12 +0200

    updating account list page after activating user

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -5,6 +5,7 @@ import accountAPI from "../apis/accountAPI";
 const Accounts = () => {
   const [registeredUsers, setRegisteredUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
     fetchRegisterUser();
   }, []);
@@ -20,46 +21,52 @@ const Accounts = () => {
       setLoading(false);
     }
   };
-  console.log("registeredUsers",registeredUsers)
-  return (
-    <div>
-      <div className="page-content">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-12">
-              {/* account list */}
-              <div className="row">
-                <div className="col-12">
-                  <div className="card">
-                    <div className="card-body">
-                      <h4 className="mt-0 header-title">Account List</h4>
 
-                      <table
-                        id="datatable"
-                        className="table table-bordered dt-responsive nowrap"
-                        style={{
-                          borderCollapse: "collapse",
-                          borderSpacing: 0,
-                          width: "100%",
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th>User Name</th>
-                            <th>Full Name</th>
-                            <th>Phone</th>
-                            <th>Email</th>
-                            <th>Address</th>
-                            <th>Status</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {registeredUsers.map((account, index) => {
-                            return <Account account={account} key={index} />;
-                          })}
-                        </tbody>
-                      </table>
-                    </div>
+ 
+  return (
+    <div className="page-content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-12">
+            {/* account list */}
+            <div className="row">
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-body">
+                    <h4 className="mt-0 header-title">Account List</h4>
+
+                    <table
+                      id="datatable"
+                      className="table table-bordered dt-responsive nowrap"
+                      style={{
+                        borderCollapse: "collapse",
+                        borderSpacing: 0,
+                        width: "100%",
+                      }}
+                    >
+                      <thead>
+                        <tr>
+                          <th>User Name</th>
+                          <th>Full Name</th>
+                          <th>Phone</th>
+                          <th>Email</th>
+                          <th>Address</th>
+                          <th>Status</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {registeredUsers.map((account, index) => {
+                          return (
+                            <Account
+                              account={account}
+                              key={index}
+                              updateList={fetchRegisterUser}
+                            />
+                          );
+                        })}
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
