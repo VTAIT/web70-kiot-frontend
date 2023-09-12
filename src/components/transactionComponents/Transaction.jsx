0fb0e32f1c4ad48438dfc01f951ce6b977ff513b
Lan Pham <lan.pham@dfs.de>
2023-09-12 08:49:30 +0200

    adding Transactions page

---
+++ src/components/transactionComponents/Transaction.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaRegPenToSquare } from "react-icons/fa6";
+
+const Transaction = (props) => {
+  const transaction = props.transaction;
+
+  return (
+    <tr>
+      <td>{transaction.username}</td>
+      <td>{transaction.kiot_id}</td>
+      <td>{transaction.status}</td>
+      <td>{transaction.deposit}</td>
+      <td>{transaction.returnV}</td>
+      <td>
+        {transaction.retrun_list.map((returnedItem) => {
+          return (
+            <div>
+              {returnedItem.name}:{returnedItem.value}
+            </div>
+          );
+        })}
+      </td>
+      <td>
+        {transaction.product_list.map((item) => {
+          return (
+            <div>
+              {item.name}:{item.value}
+            </div>
+          );
+        })}
+      </td>
+      <td>{transaction.code}</td>
+      <td>{transaction.createdAt}</td>
+
+      <td>
+        {transaction.status !== 2 && (
+          <span onClick={props.onEditTransaction}>
+            <FaRegPenToSquare className="text-info ms-2" />
+          </span>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+export default Transaction;
