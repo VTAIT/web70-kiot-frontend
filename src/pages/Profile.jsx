969c2126f89b0ceb011cc40ce14a4d97a7de9ee5
Lan Pham <lan.pham@dfs.de>
2023-09-19 15:45:54 +0200

    adding Profile items to array map

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,12 @@ import * as Yup from "yup";
 import AuthContext from "../contexts/AuthContext/AuthContext";
 import kiotAPI from "../apis/kiotAPI";
 import accountAPI from "../apis/accountAPI";
+import {
+  kiotProfileModalFormItems,
+  passwordProfileModal,
+  profileModalFormItems,
+} from "../global/profileModalFormItems";
+import ModalFormItem from "../global/ModalFormItem";
 
 function Profile() {
   const [loading, setLoading] = useState(false);
@@ -204,127 +210,25 @@ function Profile() {
                   <div className="d-flex justify-content-between align-items-center mb-3">
                     <h4 className="text-right">Profile Settings</h4>
                   </div>
-                  <div className="row mt-2">
-                    <div className="col-md-12">
-                      <label className="labels">Full Name</label>
-                      <Field
-                        name="fullName"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.fullName && touched.fullName
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="fullName"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <label className="labels">Email</label>
-                      <Field
-                        name="email"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.email && touched.email ? " is-invalid" : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="email"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <label className="labels">Phone Number</label>
-                      <Field
-                        name="phone"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.phone && touched.phone ? " is-invalid" : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="phone"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <label className="labels">Address</label>
-                      <Field
-                        name="address"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.address && touched.address
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="address"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
+                  {profileModalFormItems.map((item) => (
+                    <ModalFormItem
+                      item={item}
+                      errors={errors}
+                      touched={touched}
+                    />
+                  ))}
 
                   <div>
                     <h3 className="pt-3 fs-6 fw-bold">Change Password</h3>
                     <p>Leave blank to keep the same password</p>
                   </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <label className="labels">Password</label>
-                      <Field
-                        name="password"
-                        type="password"
-                        className={
-                          "form-control" +
-                          (errors.password && touched.password
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="password"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <label className="labels">Password</label>
-                      <Field
-                        name="confirmPassword"
-                        type="password"
-                        className={
-                          "form-control" +
-                          (errors.confirmPassword && touched.confirmPassword
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="confirmPassword"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                  </div>
+                  {passwordProfileModal.map((item) => (
+                    <ModalFormItem
+                      item={item}
+                      errors={errors}
+                      touched={touched}
+                    />
+                  ))}
                   <div className="mt-5 text-center">
                     <button
                       className="btn btn-primary profile-button"
@@ -341,97 +245,13 @@ function Profile() {
                     <span className="fs-6 fw-bold px-3 p-1 add-experience">
                       Kiot Information
                     </span>
-                    <div className="col-md-12 mt-3">
-                      <label className="labels">Kiot Name</label>
-                      <Field
-                        name="kiotName"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.kiotName && touched.kiotName
-                            ? " is-invalid"
-                            : "")
-                        }
+                    {kiotProfileModalFormItems.map((item) => (
+                      <ModalFormItem
+                        item={item}
+                        errors={errors}
+                        touched={touched}
                       />
-                      <ErrorMessage
-                        name="kiotName"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>{" "}
-                    <div className="col-md-12 mt-3">
-                      <label className="labels">Kiot Phone</label>
-                      <Field
-                        name="kiotPhone"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.kiotPhone && touched.kiotPhone
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="kiotPhone"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>{" "}
-                    <div className="col-md-12 mt-3">
-                      <label className="labels">Kiot Address</label>
-                      <Field
-                        name="kiotAddress"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.kiotAddress && touched.kiotAddress
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="kiotAddress"
-                        component="div"
-                        className="invalid-feedback"
-                      />{" "}
-                    </div>{" "}
-                    <div className="col-md-12 mt-3">
-                      <label className="labels">Kiot Email</label>
-                      <Field
-                        name="kiotEmail"
-                        type="text"
-                        className={
-                          "form-control" +
-                          (errors.kiotEmail && touched.kiotEmail
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="kiotEmail"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>{" "}
-                    <div className="col-md-12 mt-3">
-                      <label className="labels">Kiot Description</label>
-                      <Field
-                        name="description"
-                        as="textarea"
-                        rows="4"
-                        className={
-                          "form-control" +
-                          (errors.description && touched.description
-                            ? " is-invalid"
-                            : "")
-                        }
-                      />
-                      <ErrorMessage
-                        name="description"
-                        component="div"
-                        className="invalid-feedback"
-                      />
-                    </div>
+                    ))}
                   </div>
                 </div>
               )}
