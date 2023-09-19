5e87e801b9b02ecbd533244d90e34765d9854a84
Nguyễn Trọng Nhân <nhannguyenvh79@gmail.com>
2023-09-19 18:09:51 +0700

    fix css payment

--- src/components/productComponents/TablePrice.jsx
+++ src/components/productComponents/TablePrice.jsx
@@ -21,41 +21,41 @@ const TablePrice = () => {
     setArrQuantity(cart.map((el) => el.quantity));
   }, [cart]);
 
-  if (!cart.length) {
-    return (
-      <>
-        <div className="table  m-0 ">
-          <div
-            className="border-bottom"
-            style={{ position: "sticky", top: "0" }}
-          >
-            <div className="grid-table-price">
-              <div className="product-grid grid-head-color">
-                <strong>Product</strong>
-              </div>
-              <div className="quantity-grid grid-head-color">
-                <strong>Quantity</strong>
-              </div>
-              <div className="total-grid grid-head-color">
-                <strong>Total</strong>
-              </div>
-              <div className="action-grid grid-head-color">
-                <strong>Action</strong>
-              </div>
-            </div>
-          </div>
-        </div>
+  // if (!cart.length) {
+  //   return (
+  //     <>
+  //       <div className="table  m-0 ">
+  //         <div
+  //           className="border-bottom"
+  //           style={{ position: "sticky", top: "0" }}
+  //         >
+  //           <div className="grid-table-price">
+  //             <div className="product-grid grid-head-color">
+  //               <strong>Product</strong>
+  //             </div>
+  //             <div className="quantity-grid grid-head-color">
+  //               <strong>Quantity</strong>
+  //             </div>
+  //             <div className="total-grid grid-head-color">
+  //               <strong>Total</strong>
+  //             </div>
+  //             <div className="action-grid grid-head-color">
+  //               <strong>Action</strong>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
 
-        <div className="w-100 h-75 bg-white position-relative">
-          <div className="position-absolute top-50 start-50 translate-middle">
-            <h6 className="text-center" style={{ color: "rgb(142, 184, 227)" }}>
-              Please choose product
-            </h6>
-          </div>
-        </div>
-      </>
-    );
-  }
+  //       <div className="w-100 position-relative">
+  //         <div className="position-absolute top-50 start-50 translate-middle">
+  //           <h6 className="text-center" style={{ color: "rgb(142, 184, 227)" }}>
+  //             Please choose product
+  //           </h6>
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <div className="table m-0 ">
@@ -77,54 +77,63 @@ const TablePrice = () => {
       </div>
 
       <div>
-        {cart.map((el, index) => {
-          return (
-            <div
-              className="grid-table-price border-bottom"
-              key={el.product._id}
-            >
-              <div className="product-grid">
-                <div className="d-flex flex-column">
-                  <img src={el.product.image} alt={el.product.name_product} />
-                  <div className="d-inline-block align-middle mb-0">
-                    <div className="d-inline-block align-middle mb-0 grid-text">
-                      {el.product.name_product}
+        {!cart.length ? (
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <h6 className="text-center" style={{ color: "rgb(142, 184, 227)" }}>
+              Please choose product
+            </h6>
+          </div>
+        ) : (
+          cart.map((el, index) => {
+            return (
+              <div
+                className="grid-table-price border-bottom"
+                key={el.product._id}
+              >
+                <div className="product-grid">
+                  <div className="d-flex flex-column">
+                    <img src={el.product.image} alt={el.product.name_product} />
+                    <div className="d-inline-block align-middle mb-0">
+                      <div className="d-inline-block align-middle mb-0 grid-text">
+                        {el.product.name_product}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="quantity-grid">
-                <input
-                  className="form-control w-75"
-                  type="number"
-                  value={arrQuantity[index]}
-                  onChange={(e) => handleOnChange(e.target.value, index)}
-                  onBlur={(e) => {
-                    changeQuantity(el.product, arrQuantity[index]);
-                  }}
-                />
-                <div className="text-muted font-13">
-                  x ${caculatePromotionPrice(el.product.price, el.saleOff)}
+                <div className="quantity-grid">
+                  <input
+                    className="form-control w-75"
+                    type="number"
+                    value={arrQuantity[index]}
+                    onChange={(e) => handleOnChange(e.target.value, index)}
+                    onBlur={(e) => {
+                      changeQuantity(el.product, arrQuantity[index]);
+                    }}
+                  />
+                  <div className="text-muted font-13">
+                    x ${caculatePromotionPrice(el.product.price, el.saleOff)}
+                  </div>
+                </div>
+
+                <div className="total-grid grid-text">
+                  $
+                  {caculateTotalPrice(
+                    caculatePromotionPrice(el.product.price, el.saleOff),
+                    el.quantity
+                  )}
+                </div>
+
+                <div className="action-grid">
+                  <button onClick={() => changeQuantity(el.product, 0)}>
+                    <AiFillCloseCircle />
+                  </button>
                 </div>
               </div>
-
-              <div className="total-grid grid-text">
-                $
-                {caculateTotalPrice(
-                  caculatePromotionPrice(el.product.price, el.saleOff),
-                  el.quantity
-                )}
-              </div>
-
-              <div className="action-grid">
-                <button onClick={() => changeQuantity(el.product, 0)}>
-                  <AiFillCloseCircle />
-                </button>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
+        {}
       </div>
     </div>
   );
